4d5bac840ba0567384b5f33f6f15a80a8c16eb1f
Wahyu Studiawan <manusiasampah7@gmail.com>
2023-04-25 04:21:57 +0700

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -65,7 +65,7 @@ ZIPNAME=Uranium
 FINAL_ZIP=${ZIPNAME}-${VERSION}-${DEVICE}-RC1.0-KERNEL-AOSP-${TM}.zip
 
 # Specify compiler [ proton, nexus, aosp ]
-COMPILER=zyc
+COMPILER=nexus
 
 # Clone ToolChain
 function cloneTC() {
